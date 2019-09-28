f7bf2da06eb5153b00afa0dbc4eb593e8ea04122
supersonic <guptasanchit16@gmail.com>
2019-09-29 03:46:37 +0530

    Update settings.py

--- dapjak/settings.py
+++ dapjak/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 import os
-import dj_database_url
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
